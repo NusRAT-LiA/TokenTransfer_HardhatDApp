bb88d3653c110cb8c6da10b8e0e489f58765cae8
NusRAT-Lia <nusratlia424@gmail.com>
2024-01-17 20:24:23 +0600

    Scripts compiled

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
+
+
+const { PRIVATE_KEY, PROJECT_ID , ETHERSCAN_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${PROJECT_ID}`,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    }},
+  etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+    },
   solidity: "0.8.19",
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,37 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Get the contract factory
+  const MyToken = await ethers.getContractFactory("MyToken");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Deploy the contract
+  console.log("Deploying contract...");
+  const myToken = await MyToken.deploy();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
 
-  await lock.waitForDeployment();
+  // Print the contract address
+  console.log(`Contract deployed to: ${myToken.target}`);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  process.exit(0);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }}
+
+// Run the main function
 main().catch((error) => {
   console.error(error);
-  process.exitCode = 1;
+  process.exit(1);
 });
